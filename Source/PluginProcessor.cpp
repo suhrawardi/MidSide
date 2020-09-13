9c4053edca87c42ca9dc144f0df4b79ffc5eeec1
jarra <suhrawardi@gmail.com>
2020-09-13 21:19:14 +0200

    Might work

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -50,8 +50,7 @@ double MidSideAudioProcessor::getTailLengthSeconds() const
 
 int MidSideAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
 }
 
 int MidSideAudioProcessor::getCurrentProgram()
@@ -87,13 +86,9 @@ void MidSideAudioProcessor::releaseResources()
 
 bool MidSideAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
     return true;
@@ -105,33 +100,27 @@ void MidSideAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
+    auto* channelDataM = buffer.getWritePointer(0);
+    auto* channelDataS = buffer.getWritePointer(1);
 
-        // ..do something to the data...
+    for (int i = 0; i < buffer.getNumSamples(); i++)
+    {
+        auto inputM = channelDataM[i];
+        auto inputS = channelDataS[i];
+        auto leftOut = inputM + inputS / 2.f;
+        auto rightOut = inputM - inputS / 2.f;
+        channelDataM[i] = leftOut;
+        channelDataS[i] = rightOut;
     }
 }
 
 //==============================================================================
 bool MidSideAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* MidSideAudioProcessor::createEditor()
@@ -153,8 +142,6 @@ void MidSideAudioProcessor::setStateInformation (const void* data, int sizeInByt
     // whose contents will have been created by the getStateInformation() call.
 }
 
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new MidSideAudioProcessor();
